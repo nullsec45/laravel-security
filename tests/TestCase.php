ed561e81ebab23679799c91a8846e55e2bc8e74c
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-09 00:21:20 +0700

    initial project

---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\DB;
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication;
+
+    protected function setUp(): void{
+        parent::setUp();
+        DB::delete("DELETE FROM users");
+    }
+}
