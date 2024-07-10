1b4735460387e737545436cef3ae7abf43568fa9
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-10 21:51:07 +0700

    test : testLogin

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Database\Seeders\UserSeeder;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthTest extends TestCase
 {
@@ -22,5 +23,27 @@ class AuthTest extends TestCase
         $user=Auth::user();
         self::assertNotNull($user);
         self::assertEquals("fajar@gmail.com", $user->email);
+    }
+
+    public function testLogin(){
+        $this->seed(UserSeeder::class);
+
+        $this->get("/users/login?email=fajar@gmail.com&password=RahasiaBanget")
+            ->assertRedirect("/users/current");
+        
+        $this->get("/users/login?email=wrong&password=wrong") 
+             ->assertSeeText("Wrong Credentials");      
+    }
+
+    public function testCurrent(){
+        $this->seed(UserSeeder::class);
+
+        $this->get("/users/current")
+             ->assertSeeText("Hello World");
+
+        $user=User::where("email","fajar@gmail.com")->first();
+        $this->actingAs($user)
+             ->get("/users/current")
+             ->assertSeeText("Hello Rama Fajar");
     }
 }
