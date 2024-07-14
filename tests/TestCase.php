2fcbed954b5e0116351d01744fbc9c0ea0ad3ae6
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-14 10:36:58 +0700

    feat: delete contacts before test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,6 +11,7 @@ abstract class TestCase extends BaseTestCase
 
     protected function setUp(): void{
         parent::setUp();
+        DB::delete("DELETE FROM contacts");
         DB::delete("DELETE FROM users");
     }
 }
