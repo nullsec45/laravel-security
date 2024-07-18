c97830c8bdacd58c236e09dae7f122107f722ab3
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-18 23:42:04 +0700

    feat : new route api/todo

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\{ProfileController, UserController};
+use App\Http\Controllers\{ProfileController, UserController,TodoController};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post("/api/todo", [TodoController::class, "create"]);
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
