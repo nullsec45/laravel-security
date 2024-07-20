f1c87e40de9231a06b70b948bf32f5ec8cf3afdc
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-20 14:07:50 +0700

    test: test view UserControllerTest

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -5,8 +5,10 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use Database\Seeders\UserSeeder;
-use App\Models\User;
+use App\Models\{User, Todo};
+use Illuminate\Support\Facades\Auth;
+use Database\Seeders\{UserSeeder, TodoSeeder};
+
 
 class UserControllerTest extends TestCase
 {
@@ -21,5 +23,34 @@ class UserControllerTest extends TestCase
         $this->actingAs($user)
              ->get("/users/current")
              ->assertSeeText("Hello Rama Fajar");
+    }
+
+    public function testView(){
+        $this->seed([UserSeeder::class, TodoSeeder::class]);
+
+        $user=User::where("email","fajar@gmail.com")->firstOrFail();
+        Auth::login($user);
+
+        $todos=Todo::query()->get();
+
+        $this->view("todos", [
+            "todos" => $todos
+        ])->assertDontSeeText("No Edit")
+          ->assertSeeText("Edit")
+          ->assertDontSeeText("No Delete")
+          ->assertSeeText("Delete");
+    }
+
+    public function testViewGuest(){
+        $this->seed([UserSeeder::class, TodoSeeder::class]);
+
+        $user=User::where("email","fajar@gmail.com")->firstOrFail();
+
+        $todos=Todo::query()->get();
+
+        $this->view("todos", [
+            "todos" => $todos
+        ])->assertSeeText("No Edit")
+          ->assertSeeText("No Delete");
     }
 }
